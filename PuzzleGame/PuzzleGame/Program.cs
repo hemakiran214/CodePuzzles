fdd8d21b1647706ce27b5269dcd9951d36a3dd2a
Hemakiran <hemakiran214@hotmail.com>
2016-08-05 11:09:26 +0530

    Add files via upload
    Puzzle Description: Watson gives Sherlock an array  A of length N. Then he asks him to determine if there exists an element in the array such that the sum of the elements on its left is equal to the sum of the elements on its right. If there are no elements to the left/right, then the sum is considered to be zero. 
    Formally, find an i , such that, A1 + A2 ... + Ai-1 =  Ai+1 + Ai+2 ... + AN.
    Input Format :
    Array of N positive integers.
    Constraints :
    1<= N <=105
    1<= Ai <= 2*104
    1<= i <= N
    Sample Input
    Input 1: 1,2,3
    Input 2: 11,20,8,31
    Sample Output
    TEST CASE: FAILED
    TEST CASE: PASSED
    Explanation 
    For the first test case, no such index exists. 
    For the second test case,A[1] + A[2] = A[4] , therefore index 3 satisfies the given conditions.
    ** <= denotes less than and equal to.

---
+++ PuzzleGame/PuzzleGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PuzzleGame
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("----------------- Code Challenge --------------------");
+            Console.WriteLine("Please enter an array of numbers seperated by comma");
+            var input = Console.ReadLine().Split(',');
+            var array = Array.ConvertAll(input, int.Parse);
+            var len = array.Length;
+            var lhsPrevSum = 0;
+            var rhsPrevSum = 0;
+            var lhs = new List<int>();
+            var rhs = new Stack<int>();
+            if (len > 2)
+            {
+                bool elementFound = false;
+                for (int i = 0, j = len; i < len - 2 && j > 2; i++, j--)
+                {
+                    lhs.Add(lhsPrevSum + array[i]);
+                    lhsPrevSum = lhs[i];
+                    rhs.Push(rhsPrevSum + array[j - 1]);
+                    rhsPrevSum = rhs.Peek();
+                }
+                if (lhs.Count == rhs.Count)
+                {
+                    foreach (int val in lhs)
+                    {
+                        if (val == rhs.Pop())
+                        {
+                            elementFound = true;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Some thing went wrong in calculations...Check ur code...");
+                }
+                Console.WriteLine("TEST CASE {0}", elementFound ? "PASSED" : "FAILED");
+            }
+            else
+            {
+                Console.WriteLine("The Input needs to be greater than two elements.");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
